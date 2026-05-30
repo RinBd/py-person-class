5fa19d4e4a3373d7b3fe500ac4661601c46b9806
Rin Baida <irinebd8@gmail.com>
2026-05-30 17:00:32 +0300

    Solution is added

--- app/main.py
+++ app/main.py
@@ -1,8 +1,24 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        self.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    list_of_objects = []
+    for person_dict in people:
+        person = Person(person_dict["name"], person_dict["age"])
+        if person_dict.get("wife") is not None:
+            person.wife = person_dict.get("wife")
+        if person_dict.get("husband") is not None:
+            person.husband = person_dict.get("husband")
+        list_of_objects.append(person)
+    for objects in list_of_objects:
+        if hasattr(objects, "wife"):
+            objects.wife = objects.people[objects.wife]
+        if hasattr(objects, "husband"):
+            objects.husband = objects.people[objects.husband]
+    return list_of_objects
